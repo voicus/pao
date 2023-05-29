9d0400ae8f244fa2901df57aaf1fcb701e3b8967
voicus <49780257+voicus@users.noreply.github.com>
2023-05-29 12:30:06 +0300

    student account

--- src/StudentAccount.java
+++ src/StudentAccount.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class StudentAccount {
+public class StudentAccount extends Account{
+  public StudentAccount(String IBAN, String swift, double amount, String name, int customerId) {
+    super(IBAN, swift, amount, name, customerId);
+  }
+
+  public StudentAccount(String name, int customerId) {
+    super(name, customerId);
+  }
 }
